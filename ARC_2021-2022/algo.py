5760f6f8b5fec6b408050d7669ded960b239cde9
m0e <lusylover45@gmail.com>
2022-01-29 09:56:47 -0500

    aded these two

---
+++ ARC_2021-2022/algo.py
@@ -0,0 +1,19 @@
+import math
+import numpy as np
+
+def determinePath(waypoint):
+	# COMPLETE HERE
+
+	#PLACEHOLDER, just send out current list2
+	order = [0,1,2,3,4]
+
+
+
+
+
+
+
+
+
+
+	return order
